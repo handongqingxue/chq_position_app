1fde74317ede8593216977cf2483d05dd98032d8
handongqignxue <1417140290@qq.com>
2020-05-26 15:45:41 +0800

    完善

--- src/pages/testDataTj/index.js
+++ src/pages/testDataTj/index.js
@@ -6,7 +6,7 @@ import ReactEcharts from "echarts-for-react";
 import Super from "../../super";
 
 class TestDataTj extends Component{
-    state={menuId:17,legendData:[],日期:68,年:74,月:72,月度周:69,日:71}
+    state={menuId:17,legendData:[],xAxisData:[],series:[],日期:68,年:74,月:72,月度周:69,日:71,报警类型:73,数量:75}
 
     componentDidMount() {
         $("html").css("background-color","#fff");
@@ -19,10 +19,10 @@ class TestDataTj extends Component{
         Super.super({
             url:`api2/entity/${this.state.menuId}/list/tmpl`,
             method:'GET',
-            query:{disabledColIds:"",criteria_13:"2020-05-20~2020-06-20"}
+            query:{disabledColIds:"",criteria_13:"2019-05-20~2020-06-20"}
         }).then((res) => {
             console.log(res);
-            if(!reload){
+            if(!reload){//这里是初始化报警类型，只有在首次加载页面的时候初始化一次就行
                 res.ltmpl.criterias.map((item,index)=>{
                     if(item.id==6){
                         this.initlegendData(item.fieldId);
@@ -38,6 +38,7 @@ class TestDataTj extends Component{
             method:'GET',
         }).then((res) => {
             console.log("==="+JSON.stringify(res));
+            this.initXAxisData(res);
         })
     }
     initlegendData=(fieldId)=>{
@@ -48,11 +49,29 @@ class TestDataTj extends Component{
         }).then((res) => {
             console.log(res.optionsMap[fieldId]);
             let ldMap=[];
+            let series=[];
             res.optionsMap[fieldId].map((item,index)=>{
                 ldMap.push(item.title);
+                series.push({name:item.title,type:'bar',data:[],barGap:0});
             });
             this.setState({legendData:ldMap});
+            this.setState({series:series});
         })
+    }
+    initXAxisData=(res)=>{
+        let xAxisData=[];
+        res.entities.map((item,index)=>{
+            let cellMap=item.cellMap;
+            //console.log(cellMap);
+            xAxisData.push(cellMap[this.state.日期]);
+            this.state.series.map((sItem,sIndex)=>{
+                if(cellMap[this.state.报警类型]==sItem.name){
+                    sItem.data.push(cellMap[this.state.数量]);
+                }
+            });
+        });
+        //console.log(this.state.series);
+        this.setState({xAxisData:xAxisData});
     }
     getOption =()=> {
         let option = {
@@ -66,12 +85,19 @@ class TestDataTj extends Component{
                 data:this.state.legendData
             },
             xAxis:{
-                data:['周一','周二','周三','周四','周五','周六','周日']
+                //data:['周一','周二','周三','周四','周五','周六','周日']
+                data:this.state.xAxisData,
+                axisLabel: {
+                    interval:0,
+                    rotate:90
+                }
             },
             yAxis:{
                 type:'value'
             },
-            series:[
+            series:this.state.series
+                /*
+                [
                 {
                     name:'人员一键紧急报警',
                     type:'bar',
@@ -83,28 +109,9 @@ class TestDataTj extends Component{
                     type:'bar',
                     data:[1000,2000,1500,3000,2000,1200,800],
                     barGap:0
-                },
-                {
-                    name:'车间超员报警',
-                    type:'bar',
-                    data:[1000,2000,1500,3000,2000,1200,800]
-                },
-                {
-                    name:'人员串岗报警',
-                    type:'bar',
-                    data:[1000,2000,1500,3000,2000,1200,800]
-                },
-                {
-                    name:'人员滞留报警',
-                    type:'bar',
-                    data:[1000,2000,1500,3000,2000,1200,800]
-                },
-                {
-                    name:'人员长时间静止报警',
-                    type:'bar',
-                    data:[1000,2000,1500,3000,2000,1200,800]
                 }
             ]
+                 */
         }
         return option;
     }
@@ -117,9 +124,9 @@ class TestDataTj extends Component{
             <div className="top_div">报警统计</div>
             <div className="back_but_div" onClick={this.goPage.bind(this,'testHome')}>&lt;返回</div>
             <div>
-                <button>月</button>
+                <button>日</button>
                 <button>周</button>
-                <button>星期</button>
+                <button>月</button>
             </div>
             <ReactEcharts option={this.getOption()}/>
         </div>;
