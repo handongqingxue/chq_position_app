b13cd458c96e677f71faed35452b97782a11ac88
handongqignxue <1417140290@qq.com>
2020-05-26 14:56:08 +0800

    完善

--- src/pages/testDataTj/index.js
+++ src/pages/testDataTj/index.js
@@ -3,11 +3,56 @@ import React, {Component} from 'react'
 import "./index.less";
 import $ from "jquery";
 import ReactEcharts from "echarts-for-react";
+import Super from "../../super";
 
 class TestDataTj extends Component{
+    state={menuId:17,legendData:[],日期:68,年:74,月:72,月度周:69,日:71}
 
     componentDidMount() {
         $("html").css("background-color","#fff");
+        this.request();
+    }
+    request=()=>{
+        this.initListByMenuId(false);
+    }
+    initListByMenuId=(reload)=>{
+        Super.super({
+            url:`api2/entity/${this.state.menuId}/list/tmpl`,
+            method:'GET',
+            query:{disabledColIds:"",criteria_13:"2020-05-20~2020-06-20"}
+        }).then((res) => {
+            console.log(res);
+            if(!reload){
+                res.ltmpl.criterias.map((item,index)=>{
+                    if(item.id==6){
+                        this.initlegendData(item.fieldId);
+                    }
+                });
+            }
+            this.initListByQueryKey(res.queryKey);
+        })
+    }
+    initListByQueryKey=(queryKey)=>{
+        Super.super({
+            url:`api2/entity/list/${queryKey}/data`,
+            method:'GET',
+        }).then((res) => {
+            console.log("==="+JSON.stringify(res));
+        })
+    }
+    initlegendData=(fieldId)=>{
+        Super.super({
+            url:`api2/meta/dict/field_options`,
+            method:'GET',
+            query: {fieldIds:fieldId}
+        }).then((res) => {
+            console.log(res.optionsMap[fieldId]);
+            let ldMap=[];
+            res.optionsMap[fieldId].map((item,index)=>{
+                ldMap.push(item.title);
+            });
+            this.setState({legendData:ldMap});
+        })
     }
     getOption =()=> {
         let option = {
@@ -18,7 +63,7 @@ class TestDataTj extends Component{
                 }
             },
             legend:{
-                data:['aaa','bbb','ccc','ddd','eee','fff']
+                data:this.state.legendData
             },
             xAxis:{
                 data:['周一','周二','周三','周四','周五','周六','周日']
@@ -28,33 +73,34 @@ class TestDataTj extends Component{
             },
             series:[
                 {
-                    name:'aaa',
+                    name:'人员一键紧急报警',
                     type:'bar',
                     data:[1000,2000,1500,3000,2000,1200,800],
                     barGap:0
                 },
                 {
-                    name:'bbb',
+                    name:'车间缺员报警',
+                    type:'bar',
+                    data:[1000,2000,1500,3000,2000,1200,800],
+                    barGap:0
+                },
+                {
+                    name:'车间超员报警',
                     type:'bar',
                     data:[1000,2000,1500,3000,2000,1200,800]
                 },
                 {
-                    name:'ccc',
+                    name:'人员串岗报警',
                     type:'bar',
                     data:[1000,2000,1500,3000,2000,1200,800]
                 },
                 {
-                    name:'ddd',
+                    name:'人员滞留报警',
                     type:'bar',
                     data:[1000,2000,1500,3000,2000,1200,800]
                 },
                 {
-                    name:'eee',
-                    type:'bar',
-                    data:[1000,2000,1500,3000,2000,1200,800]
-                },
-                {
-                    name:'fff',
+                    name:'人员长时间静止报警',
                     type:'bar',
                     data:[1000,2000,1500,3000,2000,1200,800]
                 }
@@ -70,6 +116,11 @@ class TestDataTj extends Component{
         return <div className="bjInfoPage_div">
             <div className="top_div">报警统计</div>
             <div className="back_but_div" onClick={this.goPage.bind(this,'testHome')}>&lt;返回</div>
+            <div>
+                <button>月</button>
+                <button>周</button>
+                <button>星期</button>
+            </div>
             <ReactEcharts option={this.getOption()}/>
         </div>;
     }
