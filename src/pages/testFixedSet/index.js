d4e0c6d46878ba779e79adeee4be2232d76ff687
handongqignxue <1417140290@qq.com>
2020-06-03 11:54:40 +0800

    完善

--- src/pages/testFixedSet/index.js
+++ src/pages/testFixedSet/index.js
@@ -1,11 +1,14 @@
-import React, {Component} from "react";
 import {withRouter} from "react-router-dom";
+import React, {Component} from "react";
+import "./index.less";
 import Super from "../../super";
+import $ from "jquery";
 
 class TestFixedSet extends Component{
-    state={menuId:96653404938261,groupsList:[],selectList:[],fieldItemList:[]}
+    state={menuId:96653404938261,code:"99156204279701516",groupsList:[],selectList:[],fieldItemList:[]}
 
     componentDidMount(){
+        $("html").css("background-color","#EDEDED");
         this.request();
     }
     request=()=>{
@@ -43,7 +46,7 @@ class TestFixedSet extends Component{
         });
 
         Super.super({
-            url:`api2/entity/${this.state.menuId}/detail/98972186405904392`,
+            url:`api2/entity/${this.state.menuId}/detail/${this.state.code}`,
             method:'GET',
         }).then((resi)=>{
             let fieldMap=resi.entity.fieldMap;
@@ -54,32 +57,48 @@ class TestFixedSet extends Component{
         });
     }
     submit=()=>{
+        let groupsList=this.state.groupsList;
+        groupsList.map((item,index)=>{
+            let fields=item.fields;
+            fields.map((fieldItem,fieldIndex)=>{
+                if(fieldItem.type=="select")
+                    console.log($("#field_item_select"+fieldItem.id).val())
+                else
+                    console.log($("#field_item_input"+fieldItem.id).val())
+            });
+        });
+        return false;
         Super.super({
             url:`api2/entity/${this.state.menuId}/detail/normal`,
             method:'POST',
-            data:{'唯一编码':'99156204279701516','人员静止报警阈值':'1112'}
+            data:{'唯一编码':this.state.code,'人员静止报警阈值':'1113'}
         }).then((res)=>{
             console.log(JSON.stringify(res))
         })
+    }
+    goPage=(value)=>{
+        this.props.history.push(`/${value}`);
     }
     render() {
         const {groupsList,selectList,fieldItemList}=this.state
         let {itemDiv,itemFields,fieldDiv}=this.state
         return <div className="fsPage_div">
+            <div className="top_div">定位配置</div>
+            <div className="back_but_div" onClick={this.goPage.bind(this,'testHome')}>&lt;返回</div>
             <div className="groups_list_div">
                 {
                     groupsList.map((item,index)=>{
-                        return <div>
-                            <div>{item.title}</div>
+                        return <div className="groups_item_div">
+                            <div className="title_div">{item.title}</div>
                                 {
                                     item.fields.map((fieldItem,fieldIndex)=>{
-                                        return <div fieldId={fieldItem.fieldId}>
-                                            <div>{fieldItem.title}</div>
-                                            <div>{fieldItem.id}-{fieldItem.type}</div>
-                                            <div>
+                                        return <div className="field_item_div" fieldId={fieldItem.fieldId}>
+                                            <div className="title_div">{fieldItem.title}</div>
+                                            {/*<div>{fieldItem.id}-{fieldItem.type}</div>*/}
+                                            <div className="value_div">
                                                 {fieldItem.type=="select"
                                                     ?
-                                                    <select>
+                                                    <select id={'field_item_select'+fieldItem.id}>
                                                         {
                                                             selectList[fieldItem.fieldId]
                                                                 ?
@@ -91,7 +110,7 @@ class TestFixedSet extends Component{
                                                         }
                                                     </select>
                                                     :
-                                                    <div id={'field_item_div'+fieldItem.id}>{fieldItemList[fieldItem.id]}</div>
+                                                    <input id={'field_item_input'+fieldItem.id} value={fieldItemList[fieldItem.id]}></input>
                                                 }
                                             </div>
                                         </div>
@@ -101,7 +120,7 @@ class TestFixedSet extends Component{
                     })
                 }
             </div>
-            <div onClick={this.submit.bind(this)}>保存</div>
+            <div className="submitBut_div" onClick={this.submit.bind(this)}>保存</div>
         </div>;
     }
 }
