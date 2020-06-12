f08fd6b5d902c0597b4f5f4760eb575023292903
handongqignxue <1417140290@qq.com>
2020-06-12 16:32:53 +0800

    完善

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,5 @@
-import {NavLink, withRouter} from "react-router-dom";
+import {withRouter} from "react-router-dom";
 import React, {Component} from 'react'
-import Super from "../../super"
-import {Accordion, Icon, List, Toast, Popover} from "antd-mobile";
-import Storage from "../../units/storage";
 import "./index.less";
 import ssdwImg from "../../image/002.png";
 import ryzzImg from "../../image/003.png";
@@ -13,13 +10,11 @@ import dzxjImg from "../../image/007.png";
 import dwpzImg from "../../image/008.png";
 import tcdlImg from "../../image/009.png";
 import $ from 'jquery';
-const Item = Popover.Item;
 
 class Home extends Component{
     state={homeState:[],menuTreeNode:[]}
 
     componentDidMount() {
-        //this.request();
         $("html").css("background-color","#154E6C");
         this.initNavListDiv();
     }
@@ -28,62 +23,6 @@ class Home extends Component{
         nldw=nldw.substring(0,nldw.length-2);
         $("#nav_list_div").css("height",nldw*1155/650+"px");
     }
-    /*
-    request = () => {
-        Super.super({
-            url: 'api2/meta/menu/get_blocks',
-        }).then((res) => {
-            const currentBlockId=res.currentBlockId;
-            console.log(res);
-            this.setBlocks(currentBlockId,res.blocks);
-            const homeData = this.renderData(res.blocks)
-            this.setState({
-                homeData,
-                blocks:res.blocks,
-            })
-        })
-    }
-    setBlocks=(blockId,resBlocks)=>{
-        Toast.info(blockId);
-        let {blocks}=this.state
-        let blockList
-        if(resBlocks){
-            blocks=resBlocks
-        }
-        blocks.forEach((item)=>{
-            if(item.id===blockId){
-                blockList=item.l1Menus
-            }
-        })
-        const menuTreeNode=this.renderMenu(blockList);
-        const data=this.renderData(blockList);
-        Storage.menuList=data   //普通菜单存储
-        console.log(menuTreeNode.length);
-        this.setState({menuTreeNode});
-    }
-    renderMenu=(data)=>{
-        return data.map((item)=>{
-            if(item.l1Menus)
-                return <div>2:{this.renderMenu(item.l1Menus)}<Icon type="right" size="sm"/></div>
-            else
-                return <div>1:{item.title}</div>
-        });
-    }
-    renderData=(data)=>{
-        data.forEach((item) => {
-            item.value = item.id
-            item.label = item.title
-            if(item.l2Menus) {
-                item.children = item.l2Menus
-                item.children.forEach((it) => {
-                    it.value = it.id
-                    it.label = it.title
-                })
-            }
-        })
-        return data
-    }
-     */
     goPage = (value) => {
         this.props.history.push(`/${value}`)
     }
@@ -128,6 +67,7 @@ class Home extends Component{
                     </div>
                 </div>
                 </div>
+                <div className="space_div"></div>
                 <div className="bqsy_div">版权所有2018-2020辰麒数维</div>
             </div>
         );
